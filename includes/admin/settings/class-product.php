2c7cf2faaa999b2a4ed493983744208fd81f70a6
Varun <varunsridharan23@gmail.com>
2020-06-12 15:23:36 +0530

    Fixed https://wordpress.org/support/topic/default-value-set-to-yes-since-version-1-0/

--- includes/admin/settings/class-product.php
+++ includes/admin/settings/class-product.php
@@ -23,8 +23,8 @@ if ( ! class_exists( '\SPCF_WC\Admin\Settings\Product' ) ) {
 			$c1 = $builder->container( 'spcf_wc', __( 'Single Product Checkout', 'spcfwc' ) );
 			$c1->subheading( __( 'Product Level Configuration', 'spcfwc' ) );
 			$c1->select( 'is_enabled', __( 'Enable Single Product Checkout ?', 'spcfwc' ) )
-				->option( 'yes', __( 'Yes', 'spcfwc' ) )
 				->option( 'no', __( 'No', 'spcfwc' ) )
+				->option( 'yes', __( 'Yes', 'spcfwc' ) )
 				->desc_field( 'if default selected then product types that are enabled in settings will be used. ' )
 				->desc( __( 'if enabled. then users will have to purchase this product seperatly', 'spcfwc' ) );
 			$c1->add_field( 'number', 'min_qty', __( 'Minimum Required Quantity ?', 'spcfwc' ), array(
